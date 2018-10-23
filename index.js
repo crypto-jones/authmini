2590cdd68c7d2911f41b967e18ba9781bd7c847d
crypto-jones <integral.being@gmail.com>
2018-10-23 10:11:08 -0700

    Setup session middleware

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const session = require('express-session'); // added this library
 
 const db = require('./database/dbConfig.js');
 
 const server = express();
+
+const sessionConfig = {
+  secret: 'nobody-tosses.a%dwarf.!',
+  name: 'monkey', // defaults to connect.sid
+  httpOnly: true, // JS can't access this
+  resave: false,
+  saveUninitialized: false, // laws !
+  cookie: {
+    secure: false, // over httpS
+    maxAge: 1000 * 60 * 1
+  }
+};
+server.use(session(sessionConfig));
 
 server.use(express.json());
 server.use(cors());
@@ -12,14 +27,61 @@ server.get('/', (req, res) => {
   res.send('Its Alive!');
 });
 
-// protect this route, only authenticated users should see it
-server.get('/api/users', (req, res) => {
+// implemented this
+server.post('/register', (req, res) => {
+  const credentials = req.body;
+
+  const hash = bcrypt.hashSync(credentials.password, 10);
+  credentials.password = hash;
+
   db('users')
-    .select('id', 'username')
+    .insert(credentials)
+    .then(ids => {
+      const id = ids[0];
+      req.session.username = user.username;
+      res.status(201).json({ newUserId: id });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.post('/login', (req, res) => {
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        req.session.username = user.username;
+        res.status(200).json({ welcome: user.username });
+      } else {
+        res.status(401).json({ message: 'you shall not pass!' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ err });
+    });
+});
+
+// protect this route, only authenticated users should see it
+server.get('/users', protected, (req, res) => {
+  console.log(req.session);
+  db('users')
+    .select('id', 'username', 'password')
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+function protected(req, res, next) {
+  if (req.session && req.session.username) {
+    next();
+  } else {
+    res.status(401).json({ message: 'Not authorized' });
+  }
+}
 
 server.listen(3300, () => console.log('\nrunning on port 3300\n'));
