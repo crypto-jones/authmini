b8bd34cfb41ae9380317977a9c8d873e1b5e7beb
crypto-jones <integral.being@gmail.com>
2018-10-24 10:49:08 -0700

    Integrate JWT

--- index.js
+++ index.js
@@ -1,42 +1,19 @@
+// require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
-const session = require('express-session'); // added this library
-const KnexSessionStore = require('connect-session-knex')(session);
+const jwt = require('jsonwebtoken');
 
 const db = require('./database/dbConfig.js');
 
 const server = express();
 
-const sessionConfig = {
-  secret: 'nobody-tosses.a%dwarf.!',
-  name: 'monkey', // defaults to connect.sid
-  httpOnly: true, // JS can't access this
-  resave: false,
-  saveUninitialized: false, // laws !
-  cookie: {
-    secure: false, // over httpS
-    maxAge: 1000 * 60 * 10
-  },
-  store: new KnexSessionStore({
-    tablename: 'sessions',
-    sidfieldname: 'sid',
-    knex: db,
-    createTable: true,
-    clearInterval: 1000 * 60 * 60 // removes only expired sessions
-  })
-};
-server.use(session(sessionConfig));
-
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
-  res.send('Its Alive!');
-});
-
 // implemented this
-server.post('/register', (req, res) => {
+server.post('/api/register', (req, res) => {
   const credentials = req.body;
 
   const hash = bcrypt.hashSync(credentials.password, 10);
@@ -46,15 +23,33 @@ server.post('/register', (req, res) => {
     .insert(credentials)
     .then(ids => {
       const id = ids[0];
-      req.session.username = user.username;
-      res.status(201).json({ newUserId: id });
+      // query the database to get the user
+      const token = generateToken({ username: credentials.username });
+      res.status(201).json({ newUserId: id, token });
     })
     .catch(err => {
       res.status(500).json(err);
     });
 });
 
-server.post('/login', (req, res) => {
+const jwtSecret =
+  process.env.JWT_SECRET || 'add a secret to your .env file with this key';
+
+function generateToken(user) {
+  const jwtPayload = {
+    ...user,
+    hello: 'FSW13',
+    roles: ['admin', 'root']
+  };
+  const jwtOptions = {
+    expiresIn: '1h'
+  };
+
+  console.log('token from process.env', jwtSecret);
+  return jwt.sign(jwtPayload, jwtSecret, jwtOptions);
+}
+
+server.post('/api/login', (req, res) => {
   const creds = req.body;
 
   db('users')
@@ -62,8 +57,8 @@ server.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(creds.password, user.password)) {
-        req.session.username = user.username;
-        res.status(200).json({ welcome: user.username });
+        const token = generateToken(user); // new line
+        res.status(200).json({ welcome: user.username, token });
       } else {
         res.status(401).json({ message: 'you shall not pass!' });
       }
@@ -73,35 +68,48 @@ server.post('/login', (req, res) => {
     });
 });
 
-server.get('/logout', (req, res) => {
-  if (req.session) {
-    req.session.destroy(err => {
-      if (err) {
-        res.send('You cannot leave!');
-      } else {
-        res.send('good bye');
-      }
-    });
-  }
-});
-
 // protect this route, only authenticated users should see it
-server.get('/users', protected, (req, res) => {
-  console.log(req.session);
+server.get('/api/users', protected, checkRole('admin'), (req, res) => {
+  console.log('\n ** decoded token info ** \n', req.decodedToken);
   db('users')
     .select('id', 'username', 'password')
     .then(users => {
-      res.json(users);
+      res.json({ users });
     })
     .catch(err => res.send(err));
 });
 
 function protected(req, res, next) {
-  if (req.session && req.session.username) {
-    next();
+  // authentication tokens are normally sent as a header instead of the body
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        // token verification failed
+        res.status(401).json({ message: 'invalid token' });
+      } else {
+        // token is valid
+        req.decodedToken = decodedToken; // any sub-sequent middleware of route handler have access to this
+        console.log('\n** decoded token information **\n', req.decodedToken);
+        next();
+      }
+    });
   } else {
-    res.status(401).json({ message: 'Not authorized' });
+    res.status(401).json({ message: 'no token provided' });
   }
 }
 
-server.listen(3300, () => console.log('\nrunning on port 3300\n'));
+function checkRole(role) {
+  return function(req, res, next) {
+    if (req.decodedToken && req.decodedToken.roles.includes(role)) {
+      next();
+    } else {
+      res.status(403).json({ message: 'you shall not pass! forbidden' });
+    }
+  };
+}
+
+const port = process.env.PORT || 3300;
+server.listen(port, () => console.log('\nrunning on port 3300\n'));
+
+// Unhandled rejection Error: Can't set headers after they are sent.
